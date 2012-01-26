f475be31f92be26befe0471578556b78f465472f
Lilli <lilli@janrain.com>
2012-01-26 15:29:15 -0800

    Added new files to handle the sample app to capture interface

---
+++ QuickSignIn/Classes/CaptureInterface.h
@@ -0,0 +1,12 @@
+//
+//  Created by lillialexis on 1/26/12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface CaptureInterface : NSObject
+@end
---
+++ QuickSignIn/Classes/CaptureInterface.m
@@ -0,0 +1,16 @@
+//
+//  Created by lillialexis on 1/26/12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "CaptureInterface.h"
+
+
+@implementation CaptureInterface
+{
+
+}
+
+@end
